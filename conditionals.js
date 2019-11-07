367fdaf18d4357713e3e3db8509cd435ab58be22
Mitch Lum <mklum88@gmail.com>
2019-11-07 08:19:13 +0000

    Added another statement to conditionals

--- conditionals.js
+++ conditionals.js
@@ -1,14 +1,21 @@
-const temperature = 41;
+const raining = true;
+const cold = false;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine.");
+if (raining) {
+  console.log("Don't forget your umbrella!");
 }
 
-console.log("Now you're ready to go outside!");
+const temperature = 12;
+
+if (temperature < 0) {
+    console.log("Make sure you pick out a scarf!");
+  } else if (temperature < 15) {
+    console.log("Short sleeves won't cut it!");
+  } else {
+    console.log("Short sleeves are fine.");
+  }
+  
+  console.log("Now you're ready to go outside!");
 
 if (temperature < -40 || temperature > 40) {
     console.log("Maybe going outside isn't such a great idea…");
